ea5484e71231ed7f89d9eb3d9fc46a11e31637a6
Brendan Casey <brendanwalkercasey@gmail.com>
2023-01-13 15:21:19 -0700

    added print example

---
+++ chap1/print_synt.c
@@ -0,0 +1,11 @@
+#include <stdio.h> // needed for printf
+
+int main() {
+
+    printf("Name: %s,  Info:\n", "Brendan");
+    printf("\tAge: %d \t Weight: %d\n",30,200);
+    printf("\tYear: %d \t Friend: %s\n",
+            2023,"Ben");
+
+    return 0;
+}
