15a44bf1d5b2b6082cebc6587e5fb1744023ee39
Brendan Casey <brendanwalkercasey@gmail.com>
2023-01-13 14:51:31 -0700

    added lines to hello.py

--- chap1/hello.py
+++ chap1/hello.py
@@ -1,2 +1,18 @@
-print ("Hello World")
+'''
+    The Hello World Program in Python
+'''
+
+# Python math library
+from math import *
+
+
+# main function definition:
+def main():
+    # statements on their own line
+    print("Hello World")
+    print("sqrt(4) is %f" % (sqrt(4)))
+
+# call the main function:
+main()
+
 
